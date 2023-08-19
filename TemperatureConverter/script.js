9338b301314af2360c3fb878788f8af7039fc8f5
shiva9520 <119184406+shiva9520@users.noreply.github.com>
2023-08-19 13:41:34 -0700

    Update script.js

--- TemperatureConverter/script.js
+++ TemperatureConverter/script.js
@@ -1,17 +1,30 @@
-let celsius = document.getElementById("celsius");
-let fahrenheit = document.getElementById("fahrenheit");
-console.log(celsius, fahrenheit.value);
-
-
-let toFahrenheit = () => {
-  let res = (parseFloat(celsius.value) * 9) / 5 + 32;
-  fahrenheit.value = parseFloat(res.toFixed(2));
-  console.log(res);
-}
-
-
-let toCelsius = () => {
-  let res = (parseFloat(fahrenheit.value) -32) *5/9;
-  celsius.value = parseFloat(res.toFixed(2));
-  console.log(res);
-}
+ const calculate = () => {
+  let input = document.getElementById("input");
+  console.log(input.value);
+  let tempselect = document.getElementById("select");
+  let tempselected = select.options[tempselect.selectedIndex].value;
+  console.log(tempselected);
+  let toFahrenheit = (cel) => {
+    let res = (parseFloat(input.value) * 9) / 5 + 32;
+    return res;
+  };
+  let toCelsius = (fah) => {
+    let res = parseFloat((((parseFloat(input.value) - 32) * 5) / 9).toFixed(2));
+    return res;
+  };
+  if (tempselected == "cel") {
+    document.getElementById("result").innerHTML =
+      toFahrenheit(input) + "&#176;Fahrenheit";
+    console.log(
+      (document.getElementById("result").innerHTML =
+        toFahrenheit(input) + "&#176;Fahrenheit")
+    );
+  } else {
+    document.getElementById("result").innerHTML =
+      toCelsius(input) + "&#176;Celsius";
+    console.log(
+      (document.getElementById("result").innerHTML =
+        toCelsius(input) + "&#176;Celsius")
+    );
+  }
+};
